7c30abea4e9d041a352a24c80c76c2e18d3dac8d
eldridgs <eldridgs@onid.oregonstate.edu>
2015-02-01 23:36:08 -0800

    assignement 3

---
+++ cs290-assignment3/variables.js
@@ -0,0 +1,61 @@
+/*
+Input:
+a: a whole, positive number
+
+Output:
+plus5: a number that is the sum of 5 and `a`
+asString: a string that is just `a` converted to a string
+yourNumberIs: a string that says "Your Number is `x`." where `x` is replaced by
+in input `a`
+a: the original a number
+*/
+function variableModification(a) {
+  var plus5;
+  var asString;
+  var yourNumberIs;
+  //your code here
+  plus5 = 5 + a;
+  asString = a.toString();
+  yourNumberIs = "Your Number is " + a;
+  //end your code
+  return [plus5, asString, yourNumberIs, a];
+}
+
+/*
+Input:
+b: could be anything
+
+Output:
+return true if b is a primitive string value (also known as a string literal),
+false otherwise
+*/
+function isString(b) {
+  //your code here
+  var isString;
+  if (typeof b === 'string') {
+  isString = true;
+} else {
+  isString = false;
+}
+  return isString;
+  //end your code
+}
+
+/*
+Input:
+c: could be anything
+
+Output:
+return true if c is null, false otherwise
+*/
+function isNull(c) {
+  //your code here
+  var isNull;
+  if (c === null) {
+  isNull = true;
+} else {
+  isNull = false;
+}
+  return isNull;
+  //end your code
+}
