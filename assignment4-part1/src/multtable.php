272b64f1af88b3ada3b0ca964779a4c4a417db00
eldridgs <eldridgs@onid.oregonstate.edu>
2015-02-11 14:32:17 -0800

    multtable parameters working correctly

--- assignment4-part1/src/multtable.php
+++ assignment4-part1/src/multtable.php
@@ -6,5 +6,33 @@
   </head>
   <body>
     <h1>Multiplcation Table</h1>
+	<?php
+	$min_multiplicand = $_GET['min-multiplicand'];
+	$max_multiplicand = $_GET['max-multiplicand'];
+	$min_multiplier = $_GET['min-multiplier'];
+	$max_multiplier = $_GET['max-multiplier'];
+	if ( empty($min_multiplicand) )
+	  echo 'Missing minimum multiplicand parameter.</br>';
+	elseif ( !is_numeric($min_multiplicand) ) 
+	  echo 'Minimum multiplicand must be an integer.</br>';
+	if ( empty($max_multiplicand) )
+	  echo 'Missing maximum multiplicand parameter.</br>';
+    elseif ( !is_numeric($max_multiplicand) ) 
+	  echo 'Maximum multiplicand must be an integer.</br>';
+	if ( empty($min_multiplier) )
+	  echo 'Missing minimum multiplier parameter.</br>';
+	elseif ( !is_numeric($min_multiplier) ) 
+	  echo 'Minimum multiplier must be an integer.</br>';
+	if ( empty($max_multiplier) )
+	  echo 'Missing maximum multiplier parameter.</br>';
+	elseif ( !is_numeric($max_multiplier) ) 
+	  echo 'Maximum multiplier must be an integer.</br>';
+	if ( is_numeric($min_multiplicand) && is_numeric($max_multiplicand) && is_numeric($min_multiplier) && is_numeric($max_multiplier) ) { 
+	  if( $min_multiplicand > $max_multiplicand )
+	    echo 'Minimum multiplicand larger than maximum.</br>';
+	  if ( $min_multiplier > $max_multiplier )
+	    echo 'Minimum multiplier larger than maximum.</br>';
+    }
+	?>
   </body>
 </html>
